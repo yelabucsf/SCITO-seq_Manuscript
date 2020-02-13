6261d7cd1938910e26b1b40492f8437088f56053
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2020-02-13 11:55:31 -0800

    v 0.0.1 init

---
+++ scito/__init__.py
@@ -0,0 +1,3 @@
+'''
+Scito-seq package
+'''
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='scito',
+      version='0.0.1',
+      description='YeLab software to perform SCITO-seq analysis',
+      url='https://github.com/yelabucsf/SCITO-seq',
+      author='Anton Gvaihir Ogorodnikov, Ye Lab UCSF',
+      author_email='anton.ogorodnikov@ucsf.edu',
+      license='GNU V3',
+      packages=['scito',],
+      install_requires=[
+            'numpy',
+            'seaborn',
+            'matplotlib',
+            'pandas',
+      ],
+      scripts=[],
+      zip_safe=False)
