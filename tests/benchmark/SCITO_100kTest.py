6a7c4a6cb25106c87a06ffe0978ba8b786897c30
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2020-02-26 13:47:05 -0800

    v 0.1.1 benchmarking

--- tests/benchmark/SCITO_100kTest.py
+++ tests/benchmark/SCITO_100kTest.py
@@ -5,20 +5,27 @@ import seaborn as sns
 import scanpy as sc
 import pandas as pd
 
-path = '/Users/antonogorodnikov/Documents/Work/DataSci/SCITO-seq/tests/100k_pbmc_filtered_feature_bc_matrix.h5'
-verbose = True
+if __name__ == "__main__":
+    path = '/Users/antonogorodnikov/Documents/Work/DataSci/SCITO-seq/tests/100k_pbmc_filtered_feature_bc_matrix.h5'
+    verbose = True
 
-adata = sc.read_10x_h5(path, gex_only=False)
-corr_var = pd.read_csv("/Users/antonogorodnikov/Documents/Work/DataSci/SCITO-seq/tests/var.csv", index_col=0)
+    adata = sc.read_10x_h5(path, gex_only=False)
+    corr_var = pd.read_csv("/Users/antonogorodnikov/Documents/Work/DataSci/SCITO-seq/tests/var.csv", index_col=0)
 
-adata.var = corr_var
-lol = preprocess.ScitoFrame(from_scanpy=True, adata=adata)
-lol_2 = lol.detectMux(batchid_string="barcode",
-                        positiveQuantile=0.99,
-                        n_clust=None,
-                        n_init=100,
-                        kfunc="kmeans",
-                        maxneighbor=100,
-                        seed=42,
-                        keep_input=True,
-                        verbose=True)
+    adata.var = corr_var
+
+
+    def test():
+        lol = preprocess.ScitoFrame(from_scanpy=True, adata=adata)
+        lol_2 = lol.detectMux(batchid_string="barcode",
+                              positiveQuantile=0.99,
+                              n_clust=None,
+                              n_init=100,
+                              kfunc="kmeans",
+                              maxneighbor=100,
+                              seed=42,
+                              keep_input=True,
+                              verbose=True)
+
+    result =
+
